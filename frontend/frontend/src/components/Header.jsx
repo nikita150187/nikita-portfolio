85405432fe7f28642cd8c1426a779336cff7707f
nikita150187 <nikita.taware@stud.sundsgarden.se>
2025-06-02 22:18:01 +0530

    add some changes

--- frontend/frontend/src/components/Header.jsx
+++ frontend/frontend/src/components/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <h1>Nikita Taware</h1>
+     <h1 style={{ marginBottom: 0 }}>Nikita Taware</h1>
+    <h4 style={{ marginTop: 0 }}>Full Stack Developer</h4>
     <nav>
       <a href="#about">About</a> | <a href="#projects">Projects</a> | <a href="#resume">Resume</a> | <a href="#contact">Contact</a>
     </nav>
